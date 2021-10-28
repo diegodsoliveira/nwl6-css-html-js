0e87bd510686e1103127a8d694a7920e0225e7ac
Diego Oliveira <diegodsoliveira@gmail.com>
2021-10-28 19:59:04 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+/* esconde e mostra menu ao clicar no menu hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* esconde menu ao clicar em qualquer item do menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* muda header ao rolar a página */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
