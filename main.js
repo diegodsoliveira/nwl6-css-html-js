c73b85c67f338a424c376c4ca4f7fda220f6bd22
Diego Oliveira <diegodsoliveira@gmail.com>
2021-10-29 19:16:26 -0300

    responsividade
    Implementação da responsividade para dispositivos com resolução acima de 1200px

--- main.js
+++ main.js
@@ -28,3 +28,42 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/* testimonials carrousel slider */
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/* scroll reveal */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text, #about .image, #about .text, #services header, #services .card, #testimonials header, #testimonials .testimonials, #contact .text, #contact .links, footer .brand, footer .social`,
+  { interval: 100 }
+)
+
+/* show back to top button */
+const backToTop = document.querySelector('.back-to-top')
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 560) {
+    backToTop.classList.add('show')
+  } else {
+    backToTop.classList.remove('show')
+  }
+})
